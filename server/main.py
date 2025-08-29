cc7d94972603560f10b5a8647e348e9d0c3cb7da
Saied <rama.saied11@gmail.com>
2025-08-29 15:17:08 -0300

    arregle algunas cosas

--- server/main.py
+++ server/main.py
@@ -2,25 +2,25 @@ import random
 import logging
 from datetime import datetime
 from typing import Dict
-import config from Config.py
 
 from flask import Flask, render_template, requests, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+import config from Config
 
 # Logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(fkrchat)s - %(levelname)s - %(message)s'
 )
-logger = loggin.getLogger(__fkrchat__)
+logger = loggin.getLogger(__name__)
 
 
 
 
 # Flask initializer
-app = Flask(__fkrchat__)
+app = Flask(__name__)
 
 app.config.from_object(Config)
 
@@ -29,5 +29,5 @@ def index():
     return {"msg": "Flask server rinning"}
 
 
-if __fkrchat__ == "__main__":
+if __name__ == "__main__":
     app.run(debug=app.config["DEBUG"])
