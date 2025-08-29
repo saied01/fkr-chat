29b22fc9bf0f5c6f5b8aed1212edd3a2ee17fc76
Saied <rama.saied11@gmail.com>
2025-08-29 15:31:11 -0300

    movi archivos de lugar

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import logging
 from datetime import datetime
 from typing import Dict
 
-from flask import Flask, render_template, requests, session
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask import Flask, render_template, request, session, redirect
+from flask_socketio import SocketIO, join_room, leave_room, send
 from werkzeug.middleware.proxy_fix import ProxyFix
+from string import ascii_uppercase
 
-import config from Config
+from config import Config
 
 # Logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(fkrchat)s - %(levelname)s - %(message)s'
 )
-logger = loggin.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 
@@ -24,10 +25,12 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 
+socketio = SocketIO(app)
+
 @app.route("/")
 def index():
     return {"msg": "Flask server rinning"}
 
 
 if __name__ == "__main__":
-    app.run(debug=app.config["DEBUG"])
+    socketio.run(app, debug=app.config["DEBUG"])
